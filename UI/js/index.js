bbd61f25e09239f25834e01277e820086190ed61
bryan-munene <bryanmuthuri38@gmail.com>
2018-10-09 13:52:33 +0300

    [feature #161041388] refined the styles for the login page.

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,4 +1,5 @@
 function toggleaccount(){
+    // this function switches the element account in html to class active and back when called
     document.getElementById("account").classList.toggle('active');
 }
 			
