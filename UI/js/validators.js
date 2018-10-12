3a310e7dbd6f7507017ac9adefff01bf2962364f
bryan-munene <bryanmuthuri38@gmail.com>
2018-10-12 13:47:39 +0300

    [Feature #161041455] added a styles and some validation to the new item page

--- UI/js/validators.js
+++ UI/js/validators.js
@@ -29,3 +29,62 @@ function validateLoginForm() {
     }    
 }
 
+function validateAddUserForm() {
+    var email = document.forms["login"]["Email"].value;
+    if (email == "") {
+        alert("Email must be filled out");
+        return false;
+    }
+
+
+    var password = document.forms["login"]["Password"].value;
+    if (password == "") {
+        alert("Password must be filled out");
+        return false;
+    }
+
+
+    var email = document.forms["login"]["Email"].value;
+    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!regex.test(email)) {
+        alert('Please provide a valid email address');
+        return false;
+    }    
+}
+
+
+function validateAddItemForm() {
+    var Name = document.forms["items"]["Name"].value;
+    if (Name == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+
+
+    var Image = document.forms["items"]["Image"].value;
+    if (Image == "") {
+        alert("Image must uploaded");
+        return false;
+    }
+
+
+    var Price = document.forms["items"]["Price"].value;
+    if (Price == "") {
+        alert("Price must be filled out");
+        return false;
+    }
+
+
+    var Quantity = document.forms["items"]["Quantity"].value;
+    if (Quantity == "") {
+        alert("Quantity must be filled out");
+        return false;
+    }
+
+    var Categories = document.forms["items"]["Categories"].value;
+    if (Categories == "none") {
+        alert("Categories must be selected");
+        return false;
+    }
+
+}
